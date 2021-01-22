a4a06d9d5665c8ff9a255fb06bc41e0a61e368f3
Thiago Lima <36051219+thiagoolima99@users.noreply.github.com>
2021-01-22 08:56:28 -0300

    alteração path css
    Alteração do header link css/path do arquivo css linkado.

--- exercicios-php/exercicio3.php
+++ exercicios-php/exercicio3.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="_css/estilo.css">
+    <link rel="stylesheet" href="../_css/estilo.css">
     <title>Exercícios</title>
 </head>
 <body>
